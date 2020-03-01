de9be4855bf2c94c28789cdc64bd3a618dc1c11c
liuxihua <liuxihua710@gmail.com>
2020-03-02 00:38:04 +0800

    add comment component

--- src/main/java/cn/lxh/community/controller/CommentController.java
+++ src/main/java/cn/lxh/community/controller/CommentController.java
@@ -1,6 +1,6 @@
 package cn.lxh.community.controller;
 
-import cn.lxh.community.dto.CommentDTO;
+import cn.lxh.community.dto.CommentCreateDTO;
 import cn.lxh.community.dto.ResultDTO;
 import cn.lxh.community.exception.CustomizeErrorCode;
 import cn.lxh.community.model.Comment;
@@ -8,6 +8,7 @@ import cn.lxh.community.model.User;
 import cn.lxh.community.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,16 +25,19 @@ public class CommentController {
 
     @ResponseBody
     @RequestMapping(value = "/comment", method = RequestMethod.POST)
-    public Object post(@RequestBody CommentDTO commentDTO,
+    public Object post(@RequestBody CommentCreateDTO commentCreateDTO,
                        HttpServletRequest request){
         User user =(User) request.getSession().getAttribute("user");
         if(user == null){
             return ResultDTO.errorof(CustomizeErrorCode.NO_LOGIN);
         }
+        if(commentCreateDTO == null || StringUtils.isEmpty(commentCreateDTO.getContent().trim())){
+            return ResultDTO.errorof(CustomizeErrorCode.COMMENT_IS_EMPTY);
+        }
         Comment comment = new Comment();
-        comment.setParentId(commentDTO.getParentId());
-        comment.setContent(commentDTO.getContent());
-        comment.setType(commentDTO.getType());
+        comment.setParentId(commentCreateDTO.getParentId());
+        comment.setContent(commentCreateDTO.getContent());
+        comment.setType(commentCreateDTO.getType());
         comment.setGmtModified(System.currentTimeMillis());
         comment.setGmtCreate(System.currentTimeMillis());
         comment.setCommentator(user.getId());
--- src/main/java/cn/lxh/community/dto/CommentDTO.java
+++ src/main/java/cn/lxh/community/dto/CommentDTO.java
@@ -1,11 +1,19 @@
 package cn.lxh.community.dto;
 
+import cn.lxh.community.model.User;
+import com.fasterxml.jackson.databind.node.LongNode;
 import lombok.Data;
 
 @Data
 public class CommentDTO {
 
+    private Long id;
     private Long parentId;
-    private String content;
     private Integer type;
+    private Long commentator;
+    private Long gmtCreate;
+    private Long gmtModified;
+    private Long likeCount;
+    private String content;
+    private User user;
 }
